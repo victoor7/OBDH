9f0628b7439251a219c3555c615a96ebb83393f5
atcsol <atcsol@ubuntu>
2025-04-22 17:28:16 +0200

    finish prac 6

--- asw_pus_rtems_leon3_st05_12/Debug/service_libraries/crc/src/crc.d
+++
@@ -1,8 +0,0 @@
-service_libraries/crc/src/crc.o service_libraries/crc/src/crc.o:  \
- ../service_libraries/crc/src/crc.c \
-  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/service_libraries/crc/include/public/crc.h \
-  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/basic_types.h
-
-/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/service_libraries/crc/include/public/crc.h:
-
-/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/basic_types.h:
--- asw_pus_rtems_leon3_st05_12/Debug/service_libraries/edroomsl/edroomsl/src/edroomtc.d
+++
@@ -1,6 +0,0 @@
-service_libraries/edroomsl/edroomsl/src/edroomtc.o \
-  service_libraries/edroomsl/edroomsl/src/edroomtc.o:  \
- ../service_libraries/edroomsl/edroomsl/src/edroomtc.cpp \
-  ../service_libraries/edroomsl/edroomsl/include/edroomsl/edroomtc.h
-
-../service_libraries/edroomsl/edroomsl/include/edroomsl/edroomtc.h:
