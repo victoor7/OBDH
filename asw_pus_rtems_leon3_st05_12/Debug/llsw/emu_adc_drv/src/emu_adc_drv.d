cd7a7b4b8d544146265795ab0272afd26fd8d0b0
atcsol <atcsol@ubuntu>
2025-04-22 17:30:44 +0200

    Merge branch 'master' of https://github.com/victoor7/OBDH

---
+++ asw_pus_rtems_leon3_st05_12/Debug/llsw/emu_adc_drv/src/emu_adc_drv.d
@@ -0,0 +1,17 @@
+llsw/emu_adc_drv/src/emu_adc_drv.o llsw/emu_adc_drv/src/emu_adc_drv.o:  \
+ ../llsw/emu_adc_drv/src/emu_adc_drv.c \
+  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h \
+  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/config.h \
+  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/basic_types.h \
+  /home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/emu_adc_drv/include/public/adc_drv.h
+
+/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h:
+
+/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/config.h:
+
+/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+
+/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/config/include/public/basic_types.h:
+
+/home/atcsol/git/OBDH/asw_pus_rtems_leon3_st05_12/llsw/emu_adc_drv/include/public/adc_drv.h:
